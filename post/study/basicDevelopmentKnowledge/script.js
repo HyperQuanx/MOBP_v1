851c2424031a65aa706081b9f0e9c6021bcd2f3e
DESKTOP-P8ALHH3\USER <dukyong99@naver.com>
2024-09-22 21:28:21 +0900

    DirectoryConvention

---
+++ post/study/basicDevelopmentKnowledge/script.js
@@ -0,0 +1,21 @@
+let i = 1;
+
+while (i > 0) {
+  let postsLink = document.querySelector(`#bdk${i}`);
+  if (!postsLink) {
+    break;
+  }
+
+  let idValue = postsLink.id;
+
+  if (idValue.substring(3) == i) {
+    (function (i) {
+      postsLink.addEventListener("click", () => {
+        let pageNumber = i <= 9 ? `0${i}` : `${i}`;
+        location.assign(`bdk${pageNumber}.html`);
+      });
+    })(i);
+  }
+
+  i++;
+}
