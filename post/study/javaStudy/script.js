1261bedc5b9eb536b0c8ac0a8e185152ec6de2cd
DESKTOP-P8ALHH3\USER <dukyong99@naver.com>
2024-09-23 00:25:09 +0900

    JSP_StudyStart

--- post/study/javaStudy/script.js
+++ post/study/javaStudy/script.js
@@ -1,18 +1,18 @@
 let i = 1;
 
 while (i > 0) {
-  let postsLink = document.querySelector(`#jsp${i}`);
+  let postsLink = document.querySelector(`#java${i}`);
   if (!postsLink) {
     break;
   }
 
   let idValue = postsLink.id;
 
-  if (idValue.substring(3) == i) {
+  if (idValue.substring(4) == i) {
     (function (i) {
       postsLink.addEventListener("click", () => {
         let pageNumber = i <= 9 ? `0${i}` : `${i}`;
-        location.assign(`bdk${pageNumber}.html`);
+        location.assign(`java${pageNumber}.html`);
       });
     })(i);
   }
