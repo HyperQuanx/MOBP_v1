d6103bc70c25ecc026d54855a0e9c80f31f72413
HyperQuanx <qbixroqkfwk@gmail.com>
2024-09-24 21:27:38 +0900

    메인화면에 비가 와

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-const num = [1, 2, 3, 4, 5];
+// 이건 나중에 수정하자
+const textContent = "CSS는 나중에 작성하자";
+const textContainer = document.getElementById("animatedText");
 
-// map
-console.log(num.map((e) => e * 10));
+textContent.split("").forEach((char, index) => {
+  const letterDiv = document.createElement("div");
+  letterDiv.classList.add("letter");
+  letterDiv.style.animationDelay = `${index * 0.3}s`;
+  letterDiv.textContent = char;
+  textContainer.appendChild(letterDiv);
+});
+
+// 비도 나중에 수정. 이걸로 날씨 표현하면 재밌을듯?
+const rainContainer = document.getElementById("rain");
+
+let createRain = () => {
+  for (let i = 0; i < 100; i++) {
+    const drop = document.createElement("div");
+    drop.classList.add("drop");
+    drop.style.left = `${Math.random() * 100}vw`;
+    drop.style.animationDuration = `${Math.random() * 0.5 + 0.5}s`;
+    drop.style.animationDelay = `${Math.random() * 1}s`;
+    rainContainer.appendChild(drop);
+  }
+};
+
+createRain();
